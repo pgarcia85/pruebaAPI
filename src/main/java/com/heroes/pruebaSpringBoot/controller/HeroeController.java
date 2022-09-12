8df872650fd2f056d1896375c50ba18248751b37
paula <paula.garcia2@dxc.com>
2022-09-12 13:41:08 +0200

    ADD -- se crean los controladores

--- src/main/java/com/heroes/pruebaSpringBoot/controller/HeroeController.java
+++ src/main/java/com/heroes/pruebaSpringBoot/controller/HeroeController.java
@@ -1,12 +1,114 @@
 package com.heroes.pruebaSpringBoot.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.heroes.pruebaSpringBoot.error.HeroeException;
+import com.heroes.pruebaSpringBoot.model.Heroe;
+import com.heroes.pruebaSpringBoot.service.HeroeService;
 
 @RestController
 @RequestMapping("/api/heroes")
 public class HeroeController {
+
+	@Autowired
+	private HeroeService heroeService;
+
+	@GetMapping(path = "/allHeroes", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> getAllHeroes() {
+
+		Map<String, Object> response = new HashMap<String, Object>();
+		List<Heroe> listaHeroes = null;
+		try {
+			listaHeroes = heroeService.getHeroes();
+		} catch (HeroeException e) {
+			response.put("error", e.getCode());
+			response.put("mensaje", e.getMsg());
+			return new ResponseEntity<Object>(response, HttpStatus.OK);
+		}
+		response.put("listaHeroes", listaHeroes);
+		return new ResponseEntity<Object>(response, HttpStatus.OK);
+	}
+
+	@GetMapping(path = "/heroe/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> getHeroeById(@PathVariable(value = "id") Long id) {
+
+		Map<String, Object> response = new HashMap<String, Object>();
+		Optional<Heroe> h = null;
+		try {
+			h = heroeService.getHeroePorId(id);
+		} catch (HeroeException e) {
+			response.put("error", e.getCode());
+			response.put("mensaje", e.getMsg());
+			return new ResponseEntity<Object>(response, HttpStatus.OK);
+		}
+		response.put("heroe", h);
+		return new ResponseEntity<Object>(response, HttpStatus.OK);
+	}
+
+	@GetMapping(path = "/heroesByName/{param}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> getHeroeByNombre(@PathVariable(value = "param") String param) {
+
+		Map<String, Object> response = new HashMap<String, Object>();
+		List<Heroe> listaHeroes = null;
+		try {
+			listaHeroes = heroeService.getHeroesPorNombre(param);
+		} catch (HeroeException e) {
+			response.put("error", e.getCode());
+			response.put("mensaje", e.getMsg().replace("@param", param));
+			return new ResponseEntity<Object>(response, HttpStatus.OK);
+		}
+		response.put("listaHeroes", listaHeroes);
+		return new ResponseEntity<Object>(response, HttpStatus.OK);
+	}
 	
 	
+	@PutMapping(path = "/updateHeroe/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> updateHeroe(@PathVariable("id") Long id, @RequestBody Heroe heroe) {
+
+		Map<String, Object> response = new HashMap<String, Object>();
+		
+		Heroe h;
+		try {
+			h = heroeService.actualizarHeroe(id, heroe);
+		} catch (HeroeException e) {
+			response.put("error", e.getCode());
+			response.put("mensaje", e.getMsg());
+			return new ResponseEntity<Object>(response, HttpStatus.OK);
+		}
+		response.put("Heroe actualizado", h);
+		return new ResponseEntity<Object>(response, HttpStatus.OK);
+	}
+	
+	@DeleteMapping(path = "/deleteHeroe/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Object> deleteHeroe(@PathVariable("id") Long id) {
+
+		Map<String, Object> response = new HashMap<String, Object>();
+		boolean eliminado;
+		try {
+			eliminado=heroeService.eliminarHeroe(id);
+		} catch (HeroeException e) {
+			response.put("error", e.getCode());
+			response.put("mensaje", e.getMsg());
+			return new ResponseEntity<Object>(response, HttpStatus.OK);
+		}
+		response.put("Heroe eliminado", eliminado);
+		return new ResponseEntity<Object>(response, HttpStatus.OK);
+	}
+
 
 }
--- src/test/java/com/heroes/pruebaSpringBoot/controller/HeroeControllerTest.java
+++ src/test/java/com/heroes/pruebaSpringBoot/controller/HeroeControllerTest.java
@@ -1,10 +1,141 @@
 package com.heroes.pruebaSpringBoot.controller;
 
-import org.junit.jupiter.api.extension.ExtendWith;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.heroes.pruebaSpringBoot.error.HeroeException;
+import com.heroes.pruebaSpringBoot.model.Heroe;
+import com.heroes.pruebaSpringBoot.service.HeroeService;
 
+@SpringBootTest
+@TestInstance(Lifecycle.PER_CLASS)
 public class HeroeControllerTest {
+	
+	@Autowired
+	private HeroeController heroeController;
+	
+	@Autowired
+	private HeroeService heroeService;
 
+	@Test
+	@DisplayName("Consultar todos los super heroes. La lista está vacía")
+	@Transactional
+	public void getAllHeroesListaVaciaTest() throws HeroeException {
+		heroeService.getHeroes().forEach(h-> {
+			try {
+				heroeService.eliminarHeroe(h.getId());
+			} catch (HeroeException e) {
+				e.printStackTrace();
+			}
+		});
+		Map<String, Object> respuesta = (Map<String, Object>) heroeController.getAllHeroes().getBody();
+		assertEquals("No hay super heroes en el sistema", respuesta.get("mensaje"));
+
+	}
+	
+	@Test
+	@DisplayName("Consultar todos los super heroes")
+	@Transactional
+	public void getAllHeroesTest() throws HeroeException {
+
+		Map<String, Object> respuesta = (Map<String, Object>) heroeController.getAllHeroes().getBody();
+		List<Heroe> lista = (List<Heroe>) respuesta.get("listaHeroes");
+		assertEquals(5, lista.size());
+		
+	}
+	
+	@Test
+	@DisplayName("Consultar heroe por id.")
+	@Transactional
+	public void getHeroeByIdTest() throws HeroeException {
+		
+		Map<String, Object> respuesta = (Map<String, Object>) heroeController.getHeroeById(1L).getBody();
+		Optional<Heroe> heroe =  (Optional<Heroe>) respuesta.get("heroe");
+		assertEquals("superman", heroe.get().getNombre());
+	
+	}
+	
+	@Test
+	@DisplayName("Consultar heroe por id. No existe super heroe para ese id")
+	@Transactional
+	public void getHeroeByIdNoExisteTest() {
+		Map<String, Object> respuesta = (Map<String, Object>) heroeController.getHeroeById(100L).getBody();
+		assertEquals("No se ha encontrado el super héroe", respuesta.get("mensaje"));
+
+	}
+	
+	@Test
+	@DisplayName("Consultar heroe por nombre.")
+	@Transactional
+	public void getHeroeByNombreTest() throws HeroeException {
+		
+		Map<String, Object> respuesta = (Map<String, Object>) heroeController.getHeroeByNombre("man").getBody();
+		List<Heroe> heroes =  (List<Heroe>) respuesta.get("listaHeroes");
+		assertEquals(4, heroes.size());
+		
+	}
+	
+	@Test
+	@DisplayName("Consultar heroe por nombre. No existen super heroes para ese nombre")
+	@Transactional
+	public void getHeroeByNombreNoExisteTest() {
+		Map<String, Object> respuesta = (Map<String, Object>) heroeController.getHeroeByNombre("asi").getBody();
+		assertEquals("No hay super heroes que contengan en su nombre 'asi'", respuesta.get("mensaje"));
+
+	}
+	
+	
+	@Test
+	@DisplayName("Actualizar heroe")
+	@Transactional
+	public void updateHeroeTest() throws HeroeException {
+		
+		Heroe heroeMod= new Heroe("modificado");
+		Map<String, Object> respuesta = (Map<String, Object>) heroeController.updateHeroe(5L, heroeMod).getBody();
+		Heroe heroe =  (Heroe) respuesta.get("Heroe actualizado");
+		assertEquals("modificado", heroe.getNombre());
+		
+	}
+	
+	@Test
+	@DisplayName("Actualizar heroe. No existe super heroe para ese id")
+	@Transactional
+	public void updateHeroeNoExisteTest() {
+		Heroe heroeMod= new Heroe("modificado");
+		Map<String, Object> respuesta = (Map<String, Object>) heroeController.updateHeroe(10L, heroeMod).getBody();
+		assertEquals("No se ha encontrado el super héroe", respuesta.get("mensaje"));
+
+	}
+	
+	@Test
+	@DisplayName("Eliminar heroe")
+	//@Order(8)
+	@Transactional
+	public void deleteHeroeTest() throws HeroeException {
+		
+		Map<String, Object> respuesta = (Map<String, Object>) heroeController.deleteHeroe(5L).getBody();
+		assertTrue((boolean) respuesta.get("Heroe eliminado"));
+		
+	}
+	
+	@Test
+	@DisplayName("Eliminar heroe. No existe super heroe para ese id")
+	@Transactional
+	public void deleteHeroeNoExisteTest() {
+		Map<String, Object> respuesta = (Map<String, Object>) heroeController.deleteHeroe(10L).getBody();
+		assertEquals("No se ha encontrado el super héroe", respuesta.get("mensaje"));
+
+	}
 }
